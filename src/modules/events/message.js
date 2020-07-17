be05df114fc23170b4909ea368427b88851bfa31
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-07-17 04:38:39 +0200

    refactor: подключение нового типа кеширования. удалены ненужные функции

--- src/modules/events/message.js
+++ src/modules/events/message.js
@@ -1,50 +1,39 @@
-// Импортируем модули сторонних разработчиков
-const { MessageEmbed } = require("discord.js"); // Используется для отправки сообщений типа Embed
-
-// Импортируем собственные модули
 const Guild = require("../../api/models/Guild");
-const getCommand = require("../../utils/getThing");
-const { onRunError } = require("../../utils");
+const {
+  onRunError,
+  sendErrorMessage,
+  checkPermissions,
+  missingPermsError,
+} = require("../../utils");
 
-// Экспортируем функцию. В параметрах client - бот, message - объект сообщения
 module.exports = async (client, message) => {
   // Если это бот, выходим
   if (message.author.bot || message.system) return;
 
   // Проверяем на наличие сервера.
   // Так как, некоторые команды разрешено использовать в ЛС, необходимо проверить наличие сервера
-
-  // Проверяем, находится ли сервер в базе данных. Если нет, создать его и обновить кэш
-  if (message.guild && client.settings && !client.settings.find((g) => g.id == message.guild.id)) {
-    await Guild.create({ id: message.guild.id });
-    client.settings = await Guild.find({});
-  }
-
-  // Получаем настройки сервера, иначе null
-  const guildSettings =
-    message.guild && client.settings ? client.settings.find((g) => g.id == message.guild.id) : null;
+  const isGuild = !!message.guild;
+  let guildData = isGuild ? await Guild.findOne({ id: message.guild.id }).cache() : null;
+  if (isGuild && !guildData) guildData = await Guild.create({ id: message.guild.id });
 
   // Проверяем, включена ли система выдачи ролей
   if (
-    guildSettings &&
-    guildSettings.give_role.is_enabled &&
-    guildSettings.give_role.trigger_words.length !== 0
+    guildData &&
+    guildData.give_role.is_enabled &&
+    guildData.give_role.trigger_words.length !== 0
   ) {
     // Создаем регулярное выражение, включая слова-триггеры для системы
-    let systemTrigger = new RegExp(
-      `^(?:${guildSettings.give_role.trigger_words.join("|")})$`,
-      "gi"
-    );
+    let systemTrigger = new RegExp(`^(?:${guildData.give_role.trigger_words.join("|")})$`, "gi");
     if (systemTrigger.test(message.content)) {
       return require("../giveRoles/createRequest")
-        .run({ message, guildSettings, client })
+        .run({ message, guildData, client })
         .catch((warning) => onRunError({ client, warning, message }));
     }
   }
 
   // Получаем префикс бота из базы данных. По умолчанию '/'
-  const thisPrefix = guildSettings ? guildSettings.common.prefix : "/";
-  if (!message.content.startsWith(thisPrefix)) return; // Если сообщение не начинается с него, выходим
+  const thisPrefix = guildData ? guildData.common.prefix : "/";
+  if (!message.content.startsWith(thisPrefix)) return;
 
   // Заменяем массовые упоминания на обычный текст
   message.content = message.content.replace(/@everyone/g, "**everyone**");
@@ -54,19 +43,22 @@ module.exports = async (client, message) => {
   const args = message.content.slice(thisPrefix.length).trim().split(/ +/g);
 
   // Находим команду в базе данных
-  const cmd = await getCommand(client, "command", args[0].toLowerCase().normalize());
-  args.shift(); // Удаляем первый элемент из массива (команду)
+  const cmdName = args[0].toLowerCase().normalize();
+  const cmd =
+    client.commands.find((c) => c.name == cmdName || (c.aliases && c.aliases.includes(cmdName))) ||
+    false;
+  args.shift();
 
   // Если команда есть в БД
   if (cmd && !!thisPrefix) {
     // Если команда только для разработчиков, а у автора нет прав, дать ошибку
     if (!client.isDev(message.author.id) && (["dev"].includes(cmd.category) || cmd.devOnly)) {
-      message.channel.send(
-        new MessageEmbed()
-          .setColor(guildSettings.common.color)
-          .setTitle(`**У вас нет прав на использование этой команды**`)
-      );
-
+      sendErrorMessage({
+        message,
+        content: "у вас нет прав на использование этой команды",
+        member: message.member,
+        guildData,
+      });
       return console.log(
         `[Message] ${message.author.tag} попытался использовать команду для разработчиков ${
           cmd.name
@@ -84,12 +76,12 @@ module.exports = async (client, message) => {
 
       // Если команда требует использования на сервере, написать ошибку
       if (cmd.guildOnly) {
-        message.channel.send(
-          new MessageEmbed()
-            .setColor(guildSettings.common.color)
-            .setTitle(`**Эта команда доступна только на сервере**`)
-        );
-
+        sendErrorMessage({
+          message,
+          content: "эта команда доступна только на сервере",
+          member: message.member,
+          guildData,
+        });
         return console.log(
           `[Message] ${message.author.tag} использовал команду ${cmd.name}. Ошибка: команда доступна только на сервере.`
         );
@@ -104,70 +96,36 @@ module.exports = async (client, message) => {
         }`
       );
 
-      // Проверяем наличие прав у пользователя/бота
-      const verified = verifyPerms(cmd);
-      if (verified.client.length > 0)
-        return message.channel.send(missingPermission(verified.client, true));
-      if (verified.user.length > 0) return message.channel.send(missingPermission(verified.user));
+      // Проверяем наличие прав у пользователя/бота (TODO: необходим рефакторинг)
+      const has = Object.prototype.hasOwnProperty;
+      if (has.call(cmd, "userPermissions")) {
+        const missingPerms = checkPermissions(message.channel, cmd.userPermissions, message.member);
+        if (missingPerms.length > 0) {
+          missingPermsError({ message, channel: message.channel, missingPerms, isClient: false });
+        }
+      }
+      if (has.call(cmd, "clientPermissions")) {
+        const missingPerms = checkPermissions(message.channel, cmd.userPermissions, message.member);
+        if (missingPerms.length > 0) {
+          missingPermsError({ message, channel: message.channel, missingPerms });
+        }
+      }
 
       // Если команда требует NSFW у канала, а его нет, отправить ошибку
       if (cmd.nsfw && !message.channel.nsfw) {
-        return message.channel.send(
-          new MessageEmbed()
-            .setColor("#ff3333")
-            .setTitle("**🔞 | Ошибка**")
-            .setDescription("**Эта команда доступна только в NSFW каналах**")
-        );
+        return sendErrorMessage({
+          message,
+          content: "эта команда доступна только в NSFW каналах",
+          member: message.member,
+          guildData,
+          emoji: "🔞",
+        });
       }
     }
-
-    // Бот начинает писать в канал на секунду
-    message.channel.startTyping();
-    setTimeout(() => message.channel.stopTyping(true), 1000);
 
     // Запускаем команду
     return cmd
-      .run(client, message, args, guildSettings)
+      .run(client, message, args, guildData)
       .catch((warning) => onRunError({ warning, client, message }));
-  }
-
-  // Функция для проверки прав у пользователя/бота
-  function verifyPerms(command) {
-    // Создаем ссылку на метод
-    const has = Object.prototype.hasOwnProperty;
-
-    // Создаем два массива, куда будем вставлять необходимые права
-    const clientMissingPermissions = [];
-    const userMissingPermissions = [];
-
-    // Если у бота нет прав администратора на сервере, проверяем права для бота
-    if (!message.guild.me.hasPermission("ADMINISTRATOR")) {
-      if (has.call(command, "clientPermissions")) {
-        command.clientPermissions.forEach((permission) => {
-          if (!message.guild.me.hasPermission(permission, true, false, false))
-            clientMissingPermissions.push(permission);
-        });
-      }
-
-      // Если необходимо проверить права пользователя, делаем это
-      if (has.call(command, "userPermissions")) {
-        command.userPermissions.forEach((permission) => {
-          if (!message.member.hasPermission(permission, true, false, false))
-            userMissingPermissions.push(permission);
-        });
-      }
-    }
-    return {
-      client: clientMissingPermissions,
-      user: userMissingPermissions,
-    };
-  }
-
-  // Функция на отправку сообщения о нехватке прав
-  function missingPermission(permissions, client = false) {
-    return new MessageEmbed()
-      .setColor(guildSettings.common.color)
-      .setTitle(client ? `**У бота не хватает прав**` : `**У вас не хватает прав**`)
-      .setDescription(`**Необходимо: ${permissions}**`);
   }
 };
