80c365f6a8312710a36213fb14ceebeea0dbe272
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-08-06 04:52:34 +0300

    feat(constants): добавлен файл констант. список ролей проверенный

---
+++ src/lib/utils/constants.js
@@ -0,0 +1,20 @@
+exports.verifiedRoles = [
+  // Arizona Games
+  { guildID: "543799835652915241", roleID: "545564542839685120" },
+  // Arizona Scottdale
+  { guildID: "355656045600964609", roleID: "574406581513617408" },
+  // Arizona Red-Rock
+  { guildID: "470981734863994881", roleID: "654035479100129301" },
+  // Arizona Yuma
+  { guildID: "528635749206196232", roleID: "574564467346505748" },
+  // Arizona Surprise
+  { guildID: "603603887668330496", roleID: "715284102143541270" },
+  // Arizona Prescott
+  { guildID: "662725977075351552", roleID: "705750111300616232" },
+  // Rodina Northern District
+  { guildID: "477547500232769536", roleID: "715282055390560266" },
+  // Rodina Eastern District
+  { guildID: "577511138032484360", roleID: "715694332513878087" },
+  // Rodina Central District
+  { guildID: "325607843547840522", roleID: "740650109297098784" },
+];
