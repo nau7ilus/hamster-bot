628d1a3445667fc71ba42d4f23dd7f87ecf6e840
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:11:43 +0200

    refactor(request util): рефакторинг по новым правилам линтера

--- src/api/utils/request.js
+++ src/api/utils/request.js
@@ -1,8 +1,10 @@
-const axios = require("axios");
-const API_URL = "https://discord.com/api";
+'use strict';
 
-const request = async (endpoint, { method = "get", body, userData = {} } = {}) => {
-  if (!userData) throw new TypeError("Не указаны данные для входа");
+const axios = require('axios');
+const API_URL = 'https://discord.com/api';
+
+const request = async (endpoint, { method = 'get', body, userData = {} } = {}) => {
+  if (!userData) throw new TypeError('Не указаны данные для входа');
 
   const res = await axios[method](`${API_URL}${endpoint}`, {
     headers: { Authorization: `${userData.token_type} ${userData.access_token}` },
