e69f8bcb9a14cd54a3c780fae8d8b6dfcb6a92c7
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:11:23 +0200

    refactor(user scheme): рефакторинг по новым правилам линтера

--- src/api/schemas/users.js
+++ src/api/schemas/users.js
@@ -1,23 +1,25 @@
+'use strict';
+
 exports.loginUser = {
-  description: "Авторизация пользователя через Discord OAuth 2.0",
-  tags: ["users"],
-  summary: "Генерирует токен для дальнейших запросов",
+  description: 'Авторизация пользователя через Discord OAuth 2.0',
+  tags: ['users'],
+  summary: 'Генерирует токен для дальнейших запросов',
   body: {
-    type: "object",
-    required: ["code", "clientId", "redirectUri"],
+    type: 'object',
+    required: ['code', 'clientId', 'redirectUri'],
     properties: {
-      clientId: { type: "string" },
-      code: { type: "string" },
-      redirectUri: { type: "string" },
+      clientId: { type: 'string' },
+      code: { type: 'string' },
+      redirectUri: { type: 'string' },
     },
   },
   response: {
     200: {
-      description: "Успешная обработка запроса",
-      type: "object",
+      description: 'Успешная обработка запроса',
+      type: 'object',
       properties: {
-        access_token: { type: "string" },
-        token_type: { type: "string" },
+        access_token: { type: 'string' },
+        token_type: { type: 'string' },
       },
     },
   },
