9c109b6e38de47cbac9aaf24569cb773f294e956
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:13:46 +0200

    refactor(get request info): рефакторинг по новым правилам линтера

--- src/events/RoleRequests/getInfo.js
+++ src/events/RoleRequests/getInfo.js
@@ -1,14 +1,9 @@
-const { MessageEmbed } = require("discord.js");
-const { sendErrorMessage } = require("lib/utils");
+'use strict';
 
-exports.run = async ({
-  tagInfo,
-  requestInfo,
-  reaction,
-  requestAuthor,
-  guildData,
-  reactedMember,
-}) => {
+const { MessageEmbed } = require('discord.js');
+const { sendErrorMessage } = require('utils');
+
+exports.run = ({ tagInfo, requestInfo, reaction, requestAuthor, guildData, reactedMember }) => {
   const { message } = reaction;
 
   // Если не было пользователя, написать об багнутом запросе
@@ -16,7 +11,7 @@ exports.run = async ({
     // Отправим сообщение об ошибке
     sendErrorMessage({
       message,
-      content: "бот не может найти данные о запросе",
+      content: 'бот не может найти данные о запросе',
       member: reactedMember,
       react: false,
       messageType: guildData.give_role.message_type,
@@ -28,13 +23,13 @@ exports.run = async ({
   // Проверим права автора реакции
   if (
     tagInfo &&
-    !reactedMember.hasPermission("MANAGE_ROLES") &&
-    !reactedMember.roles.cache.some((role) => tagInfo.manage_roles.includes(role.id))
+    !reactedMember.hasPermission('MANAGE_ROLES') &&
+    !reactedMember.roles.cache.some(role => tagInfo.manage_roles.includes(role.id))
   ) {
     // Отправим сообщение об ошибке
     sendErrorMessage({
       message,
-      content: "у вас нет прав на управление данного запроса",
+      content: 'у вас нет прав на управление данного запроса',
       member: reactedMember,
       react: false,
       messageType: guildData.give_role.message_type,
@@ -45,54 +40,53 @@ exports.run = async ({
 
   const whatChanged = [];
   let rolesToGive = tagInfo.give_roles
-    .map((r) => (message.guild.roles.cache.get(r) ? `<@&${r}>` : "Не найдено"))
-    .join("\n");
+    .map(r => (message.guild.roles.cache.get(r) ? `<@&${r}>` : 'Не найдено'))
+    .join('\n');
   let channel = message.guild.channels.cache.get(requestInfo.requested_channel) || null;
 
   if (requestAuthor) {
-    whatChanged.push("+ Пользователь есть на сервере");
+    whatChanged.push('+ Пользователь есть на сервере');
     // Сравним ник
-    const embedNick = message.embeds[0].fields[1].value.replace(/\*/g, "") || null;
+    const embedNick = message.embeds[0].fields[1].value.replace(/\*/g, '') || null;
     if (!embedNick) {
-      whatChanged.push("- Ошибка при поиске ника в теле запроса");
+      whatChanged.push('- Ошибка при поиске ника в теле запроса');
     } else {
       if (
-        embedNick !== requestAuthor.displayName.replace(/[`|"|*]/gi, "") ||
-        embedNick !== requestInfo.user.nick_info[0].replace(/[`|"|*]/gi, "")
+        embedNick !== requestAuthor.displayName.replace(/[`|"|*]/gi, '') ||
+        embedNick !== requestInfo.user.nick_info[0].replace(/[`|"|*]/gi, '')
       ) {
-        whatChanged.push("- Ник пользователя отличается с тем, что в базе данных");
+        whatChanged.push('- Ник пользователя отличается с тем, что в базе данных');
       }
 
-      const nickRegex = new RegExp(guildData.give_role.name_regexp, "i");
+      const nickRegex = new RegExp(guildData.give_role.name_regexp, 'i');
       // Если ник не подходит по форме, отправить ошибку
       if (!nickRegex || !nickRegex.test(requestAuthor.displayName)) {
-        whatChanged.push("- Ник пользователя не проходит проверку на форму");
+        whatChanged.push('- Ник пользователя не проходит проверку на форму');
       } else {
-        whatChanged.push("+ Ник пользователя проходит проверку на форму");
+        whatChanged.push('+ Ник пользователя проходит проверку на форму');
         // Создаем массив с информацией по нику пользователя
         let nickInfo = requestAuthor.displayName.match(nickRegex);
         nickInfo[0] = requestAuthor.displayName;
 
         // Поиск тега
         const newTagInfo = guildData.give_role.tags
-          ? guildData.give_role.tags.find((tag) => tag.names.includes(nickInfo[1]))
+          ? guildData.give_role.tags.find(tag => tag.names.includes(nickInfo[1]))
           : null;
         if (newTagInfo) {
           whatChanged.push(`+ Обнаружен тег ${nickInfo[1]}`);
           requestInfo.user.nick_info = nickInfo;
 
           // Проверка ролей
-          //rolesToGive = newTagInfo.give_roles.map((r) => `<@&${r}>`).join("\n");
-          const roles = newTagInfo.give_roles.map(
-            (role) => message.guild.roles.cache.get(role) || null
-          );
-          if (roles.find((role) => role && role.editable)) {
+          const roles = newTagInfo.give_roles.map(role => message.guild.roles.cache.get(role) || null);
+          // eslint-disable-next-line max-depth
+          if (roles.find(role => role && role.editable)) {
             whatChanged.push(`+ У бота есть права для выдачи необходимых ролей`);
           } else {
             whatChanged.push(`- У бота нет прав для выдачи необходимых ролей`);
           }
 
-          const embedRoles = message.embeds[0].fields[2].value.replace(/\*/g, "") || null;
+          const embedRoles = message.embeds[0].fields[2].value.replace(/\*/g, '') || null;
+          // eslint-disable-next-line max-depth
           if (!embedRoles || rolesToGive !== embedRoles) {
             whatChanged.push(`- Обновлен список ролей для выдачи`);
             requestInfo.give_roles = newTagInfo.give_roles;
@@ -105,7 +99,7 @@ exports.run = async ({
       }
     }
   } else {
-    whatChanged.push("- Пользователя нет на сервере");
+    whatChanged.push('- Пользователя нет на сервере');
   }
   if (!channel) whatChanged.push(`- Канал отправки не найден`);
   else whatChanged.push(`+ Канал отправки найден`);
@@ -123,26 +117,26 @@ exports.run = async ({
 function editRequestMessage({ message, member, rolesToGive, channel, whatChanged }) {
   message.edit(
     new MessageEmbed()
-      .setColor("#b8ff29")
-      .setTitle("**📨 | Запрос роли**")
+      .setColor('#b8ff29')
+      .setTitle('**📨 | Запрос роли**')
       .addFields(
         { name: `**Пользователь**`, value: `**${member}**`, inline: true },
         {
           name: `**Никнейм**`,
-          value: `**${member.displayName ? member.displayName : "Не найдено"}**`,
+          value: `**${member.displayName ? member.displayName : 'Не найдено'}**`,
           inline: true,
         },
         { name: `**Роли для выдачи**`, value: `**${rolesToGive}**`, inline: true },
-        { name: `**Канал отправки**`, value: `**${channel || "Удален"}**`, inline: true },
-        { name: "**Примечания**", value: "**```diff\n" + whatChanged.join("\n") + "```**" },
+        { name: `**Канал отправки**`, value: `**${channel || 'Удален'}**`, inline: true },
+        { name: '**Примечания**', value: `**\`\`\`diff\n${whatChanged.join('\n')}\`\`\`**` },
         {
           name: `**Информация по выдаче**`,
           value:
-            "**`[✅] - выдать роль\n" +
-            "[❌] - отказать в выдачи роли\n" +
-            "[🔎] - проверить информацию\n" +
-            "[🗑️] - удалить сообщение`**",
-        }
-      )
+            '**`[✅] - выдать роль\n' +
+            '[❌] - отказать в выдачи роли\n' +
+            '[🔎] - проверить информацию\n' +
+            '[🗑️] - удалить сообщение`**',
+        },
+      ),
   );
 }
