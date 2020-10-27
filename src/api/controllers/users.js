e2f38aef9f29ae1f15feac47090eff738bd852f8
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:10:14 +0200

    refactor(user controller): рефакторинг по новым правилам линтера

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,53 +1,53 @@
-const FormData = require("form-data");
-const { sign } = require("jsonwebtoken");
-const { post } = require("axios");
+/* eslint-disable no-unused-expressions */
+'use strict';
 
-const APIError = require("lib/structures/API/ApiError");
-const Guild = require("lib/models/Guild");
-const request = require("api/utils/request");
+const { post } = require('axios');
+const FormData = require('form-data');
+const { sign } = require('jsonwebtoken');
 
-const API_URL = "https://discord.com/api";
+const Guild = require('../../models/Guild');
+const APIError = require('../errors/APIError');
+const request = require('../utils/request');
 
-/**
- * Получение данных о пользователе
- */
+const API_URL = 'https://discord.com/api';
+
+// Получение данных о пользователе
+// eslint-disable-next-line consistent-return
 exports.getUser = async (req, res) => {
-  const query = req.query && req.query.q ? req.query.q : "guilds identify";
+  const query = req.query && req.query.q ? req.query.q : 'guilds identify';
   const { userID } = req.params;
   if (!userID) {
-    return res
-      .code(400)
-      .send(new APIError({ message: "Не указан userID", status: 400, code: 20005 }));
+    return res.code(400).send(new APIError({ message: 'Не указан userID', status: 400, code: 20005 }));
   }
 
   const response = {};
-  if (userID == "@me") {
-    if (query.includes("identify")) response.identify = await _fetchUser(req.userData);
+  if (userID === '@me') {
+    if (query.includes('identify')) response.identify = await _fetchUser(req.userData);
 
-    if (query.includes("guilds")) response.guilds = await _fetchGuilds(req.userData, req.client);
-  } else
+    if (query.includes('guilds')) response.guilds = await _fetchGuilds(req.userData, req.client);
+  } else {
     res.code(403).send(
       new APIError({
-        message: "Отсутствует доступ",
+        message: 'Отсутствует доступ',
         status: 403,
         code: 20004,
         details: { userID },
-      })
+      }),
     );
+  }
 
   res.code(200).send({ status: 200, request: { userID, query }, response });
 };
 
-/**
- * Авторизация пользователя
- */
+// Авторизация пользователя
+// eslint-disable-next-line consistent-return
 exports.loginUser = async (req, res) => {
   // Проверяем наличие кода
   const { code, clientId, redirectUri } = req.body;
   if (!code || !clientId || !redirectUri) {
     return res
       .code(400)
-      .send(new APIError({ message: "Не был указан код авторизации", status: 400, code: 20001 }));
+      .send(new APIError({ message: 'Не был указан код авторизации', status: 400, code: 20001 }));
   }
 
   try {
@@ -58,16 +58,16 @@ exports.loginUser = async (req, res) => {
     if (scope !== process.env.EXPECT_SCOPES) {
       return res.code(401).send(
         new APIError({
-          message: "Получены не все данные. Возможно, исходная ссылка изменена.",
+          message: 'Получены не все данные. Возможно, исходная ссылка изменена.',
           status: 401,
           details: { received: scope, expected: process.env.EXPECT_SCOPES },
-        })
+        }),
       );
     }
 
     // Получаем ID пользователя
     const { id } = await _fetchUser({ access_token, token_type });
-    if (!id) throw new Error("Пользователь не найден");
+    if (!id) throw new Error('Пользователь не найден');
 
     // Создаем JWT и отправляем его пользователю
     const token = sign({ access_token, token_type, id }, process.env.JWT_PRIVATE);
@@ -76,26 +76,26 @@ exports.loginUser = async (req, res) => {
     console.log(err.toJSON());
     res.code(500).send(
       new APIError({
-        message: "Произошла ошибка при запросе токена у Discord",
+        message: 'Произошла ошибка при запросе токена у Discord',
         code: 20002,
         detail: err,
-      })
+      }),
     );
   }
 };
 
 function _fetchUser(userData) {
-  return request("/users/@me", { userData });
+  return request('/users/@me', { userData });
 }
 
 async function _tokenRequest(code, clientId, redirectUri) {
   const form = new FormData();
-  form.append("client_id", clientId);
-  form.append("client_secret", process.env.DISCORD_SECRET);
-  form.append("grant_type", "authorization_code");
-  form.append("redirect_uri", redirectUri);
-  form.append("scope", process.env.EXPECT_SCOPES);
-  form.append("code", code);
+  form.append('client_id', clientId);
+  form.append('client_secret', process.env.DISCORD_SECRET);
+  form.append('grant_type', 'authorization_code');
+  form.append('redirect_uri', redirectUri);
+  form.append('scope', process.env.EXPECT_SCOPES);
+  form.append('code', code);
 
   const res = await post(`${API_URL}/oauth2/token`, form, {
     headers: form.getHeaders(),
@@ -107,10 +107,10 @@ async function _tokenRequest(code, clientId, redirectUri) {
 
 async function _fetchGuilds(userData, client) {
   // Запрашиваем список серверов у пользователя
-  const guilds = await request("/users/@me/guilds", { userData });
+  const guilds = await request('/users/@me/guilds', { userData });
 
-  return await Promise.all(
-    guilds.map(async (guild) => {
+  return Promise.all(
+    guilds.map(async guild => {
       // Поиск сервера
       const clientGuild = client.guilds.cache.get(guild.id);
 
@@ -119,7 +119,8 @@ async function _fetchGuilds(userData, client) {
 
       // Список ролей
       guild.roles = guild.is_common
-        ? clientGuild.roles.cache.toJSON().map((r) => {
+        ? // eslint-disable-next-line array-callback-return
+          clientGuild.roles.cache.toJSON().map(r => {
             r.id;
             r.name;
             r.color;
@@ -131,36 +132,32 @@ async function _fetchGuilds(userData, client) {
       guild.text_channels = guild.is_common
         ? clientGuild.channels.cache
             .toJSON()
-            .filter((c) => c.type === "text" || c.type === "news")
-            .map((c) => {
-              return {
-                id: c.id,
-                name: c.name,
-                raw_position: c.rawPosition,
-                topic: c.topic,
-                nsfw: c.nsfw,
-              };
-            })
+            .filter(c => c.type === 'text' || c.type === 'news')
+            .map(c => ({
+              id: c.id,
+              name: c.name,
+              raw_position: c.rawPosition,
+              topic: c.topic,
+              nsfw: c.nsfw,
+            }))
         : null;
 
       // Список голосовых каналов
       guild.voice_channels = guild.is_common
         ? clientGuild.channels.cache
             .toJSON()
-            .filter((c) => c.type === "voice")
-            .map((c) => {
-              return {
-                id: c.id,
-                name: c.name,
-                raw_position: c.rawPosition,
-              };
-            })
+            .filter(c => c.type === 'voice')
+            .map(c => ({
+              id: c.id,
+              name: c.name,
+              raw_position: c.rawPosition,
+            }))
         : null;
 
       // Сервер премиум?
       const guildData = await Guild.findOne({ id: guild.id }).cache();
       guild.is_premium = guildData ? guildData.is_premium : false;
       return guild;
-    })
+    }),
   );
 }
