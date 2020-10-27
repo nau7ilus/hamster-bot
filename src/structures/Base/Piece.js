cf7fb854287e531ad812e4ac8882b3a111b7c6e2
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:17:26 +0200

    refactor(piece): рефакторинг по новым правилам линтера

--- src/structures/Base/Piece.js
+++ src/structures/Base/Piece.js
@@ -1,7 +1,9 @@
+'use strict';
+
 class Piece {
   constructor(store, file, directory, options) {
     this.client = store.client;
-    this.name = typeof file == "string" ? file.split(".")[0] : file;
+    this.name = typeof file === 'string' ? file.split('.')[0] : file;
     this.enabled = options ? options.enabled : true;
     this.store = store;
     this.directory = directory;
