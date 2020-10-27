e5cf2e9afc791917dc805236f3beeeb3ae2e38a4
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:15:22 +0200

    refactor(lang en): рефакторинг по новым правилам линтера

--- src/languages/en-US.js
+++ src/languages/en-US.js
@@ -1,10 +1,12 @@
-const Language = require("lib/structures/Language");
+'use strict';
+
+const Language = require('../structures/Language');
 
 module.exports = class extends Language {
   get phrases() {
     return {
-      DEFAULT: (key) => `${key} has not been localized for en-US yet.`,
-      DEFAULT_LANGUAGE: "Default Language",
+      DEFAULT: key => `${key} has not been localized for en-US yet.`,
+      DEFAULT_LANGUAGE: 'Default Language',
     };
   }
 };
