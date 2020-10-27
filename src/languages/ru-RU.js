d3af600238ba155ecd3a100f69b75b60f345c5af
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:15:30 +0200

    refactor(lang ru): рефакторинг по новым правилам линтера

--- src/languages/ru-RU.js
+++ src/languages/ru-RU.js
@@ -1,11 +1,13 @@
-const Language = require("lib/structures/Language");
+'use strict';
+
+const Language = require('../structures/Language');
 
 module.exports = class extends Language {
   get phrases() {
     return {
-      DEFAULT: (key) => `${key} еще не переведен под русский язык.`,
-      DEFAULT_LANGUAGE: "Язык по умолчанию",
-      ERR_GIF_FIND: ["**Произошла ошибка при поиске картинки.**", "**Попробуйте позже.**"],
+      DEFAULT: key => `${key} еще не переведен под русский язык.`,
+      DEFAULT_LANGUAGE: 'Язык по умолчанию',
+      ERR_GIF_FIND: ['**Произошла ошибка при поиске картинки.**', '**Попробуйте позже.**'],
     };
   }
 };
