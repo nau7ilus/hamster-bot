f75c56c46e242a0bb2a3109ef251de608d9ef009
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:10:29 +0200

    refactor(guilds controller): рефакторинг по новым правилам линтера

--- src/api/controllers/guilds.js
+++ src/api/controllers/guilds.js
@@ -1,37 +1,52 @@
-const Guild = require("lib/models/Guild");
+'use strict';
+
+const Guild = require('../models/Guild');
 
 exports.getGuildData = async (req, res) => {
   try {
     // Получение ID сервера
     const { guildId, action } = req.params;
-    if (!guildId || !action)
-      return res.code(400).send({ error: "Не указан ID сервера для поиска или действие" });
+    if (!guildId || !action) {
+      res.code(400).send({ error: 'Не указан ID сервера для поиска или действие' });
+      return;
+    }
 
     // Дальше идет повторение кода. Можно добавить утилиту для этого
     // Поиск сервера в кеше бота
     const fetchedGuild = req.client.guilds.cache.get(guildId);
-    if (!fetchedGuild) return res.code(400).send({ error: "Бота нет на данном сервере" });
+    if (!fetchedGuild) {
+      res.code(400).send({ error: 'Бота нет на данном сервере' });
+      return;
+    }
 
     // Поиск пользователя на данном сервере
     const fetchedMember = fetchedGuild.members.cache.get(req.userData.id);
-    if (!fetchedMember) return res.code(400).send({ error: "Пользователя нет на сервере" });
+    if (!fetchedMember) {
+      res.code(400).send({ error: 'Пользователя нет на сервере' });
+      return;
+    }
 
     // Проверка доступа пользователя к панели управления
-    if (!fetchedMember.hasPermission("MANAGE_GUILD"))
-      return res.code(403).send({ error: "У пользователя нет прав просматривать данную страницу" });
+    if (!fetchedMember.hasPermission('MANAGE_GUILD')) {
+      res.code(403).send({ error: 'У пользователя нет прав просматривать данную страницу' });
+      return;
+    }
 
     // Поиск сервера в базе данных
     const guildSettings = await Guild.findOne({ id: guildId });
-    if (!guildSettings) return res.code(404).send({ error: "Сервер не найден в базе данных" });
+    if (!guildSettings) {
+      res.code(404).send({ error: 'Сервер не найден в базе данных' });
+      return;
+    }
 
     const data = {
       common: guildSettings.common,
-      "give-role": guildSettings.give_role,
+      'give-role': guildSettings.give_role,
     };
 
-    return { data: data[action] || null };
+    res.send({ data: data[action] || null });
   } catch (err) {
-    return err;
+    res.code(500).send(err);
   }
 };
 
@@ -39,32 +54,51 @@ exports.changeGuildData = async (req, res) => {
   try {
     // Получение ID сервера
     const { guildId, action } = req.params;
-    if (!guildId || !action)
-      return res.code(400).send({ error: "Не указан ID сервера для поиска или действие" });
+    if (!guildId || !action) {
+      res.code(400).send({ error: 'Не указан ID сервера для поиска или действие' });
+      return;
+    }
 
     // Проверка полученных данных для изменения
     const givenData = req.body;
-    if (!givenData) return res.code(400).send({ error: "Данные для изменения не указаны" });
+    if (!givenData) {
+      res.code(400).send({ error: 'Данные для изменения не указаны' });
+      return;
+    }
 
     // Дальше идет повторение кода. Можно добавить утилиту для этого
     // Поиск сервера в кеше бота
     const fetchedGuild = req.client.guilds.cache.get(guildId);
-    if (!fetchedGuild) return res.code(400).send({ error: "Бота нет на данном сервере" });
+    if (!fetchedGuild) {
+      res.code(400).send({ error: 'Бота нет на данном сервере' });
+      return;
+    }
 
     // Поиск пользователя на данном сервере
     const fetchedMember = fetchedGuild.members.cache.get(req.userData.id);
-    if (!fetchedMember) return res.code(400).send({ error: "Пользователя нет на сервере" });
+    if (!fetchedMember) {
+      res.code(400).send({ error: 'Пользователя нет на сервере' });
+      return;
+    }
 
     // Проверка доступа пользователя к панели управления
-    if (!fetchedMember.hasPermission("MANAGE_GUILD"))
-      return res.code(403).send({ error: "У пользователя нет прав просматривать данную страницу" });
+    if (!fetchedMember.hasPermission('MANAGE_GUILD')) {
+      res.code(403).send({ error: 'У пользователя нет прав просматривать данную страницу' });
+      return;
+    }
 
     // Поиск сервера в базе данных
     const guildSettings = await Guild.findOne({ id: guildId });
-    if (!guildSettings) return res.code(404).send({ error: "Сервер не найден в базе данных" });
+    if (!guildSettings) {
+      res.code(404).send({ error: 'Сервер не найден в базе данных' });
+      return;
+    }
 
     // Нет такого типа данных в модели?
-    if (!guildSettings[action]) return res.code(400).send({ error: "Тип данных указан неверно" });
+    if (!guildSettings[action]) {
+      res.code(400).send({ error: 'Тип данных указан неверно' });
+      return;
+    }
 
     // Изменение данных
     for (let i in guildSettings[action].toJSON()) {
@@ -75,8 +109,8 @@ exports.changeGuildData = async (req, res) => {
     await guildSettings.save();
 
     // Возвращаем измененные данные
-    return guildSettings[action];
+    res.send(guildSettings[action]);
   } catch (err) {
-    return err;
+    res.code(500).send(err);
   }
 };
