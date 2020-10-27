550d845b36e2854504686302a98aa116accca08a
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:12:25 +0200

    refactor(readctionAdd event): рефакторинг по новым правилам линтера

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,5 +1,7 @@
-const Guild = require("lib/models/Guild");
-const { onRunError } = require("lib/utils");
+'use strict';
+
+const Guild = require('models/Guild');
+const { onRunError } = require('utils');
 
 module.exports = async (client, reaction, reactedUser) => {
   // Если автор бот - выходим
@@ -7,22 +9,6 @@ module.exports = async (client, reaction, reactedUser) => {
 
   // Получаем сообщение, пользователей и эмодзи
   const { message } = reaction;
-
-  // Выдача роли тестировщика/обновлений бота на сервере бота
-  if (message.guild.id === "625036675059548220") {
-    if (message.channel.id !== "685637641487777918") return;
-    if (message.author.id !== client.user.id) return;
-    if (reaction.emoji.name === "📯") {
-      const roleToGive = message.guild.roles.cache.get("685688643847061525");
-      const userForRole = message.guild.members.cache.get(reactedUser.id);
-      userForRole.roles.add(roleToGive);
-    }
-    if (reaction.emoji.name == "🙋") {
-      const roleToGive = message.guild.roles.cache.get("625318204071215151");
-      const userForRole = message.guild.members.cache.get(reactedUser.id);
-      userForRole.roles.add(roleToGive);
-    }
-  }
 
   // Проверяем на наличие сервера.
   // Так как, некоторые команды разрешено использовать в ЛС, необходимо проверить наличие сервера
@@ -35,9 +21,9 @@ module.exports = async (client, reaction, reactedUser) => {
     // Если в канале для запроса роли
     const requests_channel = message.guild.channels.cache.get(guildData.give_role.requests_channel);
     if (requests_channel && message.channel.id === requests_channel.id) {
-      require("./RoleRequests/reactionController")
+      require('./RoleRequests/reactionController')
         .run({ client, reaction, reactedUser, guildData })
-        .catch((warning) => onRunError({ client, warning, message }));
+        .catch(warning => onRunError({ client, warning, message }));
     }
   }
 };
