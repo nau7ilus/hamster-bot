9a65fb70bce1e7e9b8aea73ebe1f8ce8eb5210fa
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:13:18 +0200

    refactor(ready event): рефакторинг по новым правилам линтера

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,17 +1,21 @@
-// Импортируем модули сторонних разработчиков
-const { DateTime } = require("luxon"); // Форматирование времени
+'use strict';
 
-module.exports = async (client) => {
-  console.log(`\n[Ready] Бот запущен. Авторизован как ${client.user.tag // prettier-ignore
-		} | Серверов: ${client.guilds.cache.size} | Пользователей: ${ // prettier-ignore
-		client.users.cache.size}`) // prettier-ignore
+const { DateTime } = require('luxon');
 
-  console.log(`[Ready] Время: ${DateTime.local().toFormat("TT")}`);
+module.exports = client => {
+  console.log(
+    `\n[Ready] Бот запущен. Авторизован как %s  | Серверов: %d | Пользователей: %d`,
+    client.user.tag,
+    client.guilds.cache.size,
+    client.users.cache.size,
+  );
+
+  console.log(`[Ready] Время: ${DateTime.local().toFormat('TT')}`);
   console.log(`[Ready] RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
 
   // Отправлять каждые 20 минут информацию об использованной памяти
   setInterval(() => {
-    console.log(`\n[Ready] Время: ${DateTime.local().toFormat("TT")}`);
+    console.log(`\n[Ready] Время: ${DateTime.local().toFormat('TT')}`);
     console.log(`[Ready] RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
   }, 20 * 60 * 1000);
 };
