74a8ea892dd187e35b194b67df1a6c9245428e5d
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-28 00:13:36 +0200

    refactor(delete request): рефакторинг по новым правилам линтера

--- src/events/RoleRequests/deleteRequest.js
+++ src/events/RoleRequests/deleteRequest.js
@@ -1,26 +1,21 @@
-const { MessageEmbed } = require("discord.js");
-const { sendErrorMessage } = require("lib/utils");
+'use strict';
 
-exports.run = async ({
-  tagInfo,
-  requestInfo,
-  reaction,
-  requestAuthor,
-  guildData,
-  reactedMember,
-}) => {
+const { MessageEmbed } = require('discord.js');
+const { sendErrorMessage } = require('utils');
+
+exports.run = ({ tagInfo, requestInfo, reaction, requestAuthor, guildData, reactedMember }) => {
   // Получим сообщение и эмодзи из реакции
   const { message } = reaction;
 
   // Если не было пользователя, написать об багнутом запросе
   if (!requestAuthor || !requestInfo || !tagInfo) {
     message.channel.send(
-      guildData.give_role.message_type == "plain_text"
+      guildData.give_role.message_type === 'plain_text'
         ? `**\`[🗑️ | Удаление] \`${reactedMember}\` удалил багнутый запрос роли\`**`
         : new MessageEmbed()
-            .setColor("#b4c1d6")
-            .setTitle("**🗑️ | Удаление запроса**")
-            .setDescription(`**${reactedMember} удалил багнутый запрос роли**`)
+            .setColor('#b4c1d6')
+            .setTitle('**🗑️ | Удаление запроса**')
+            .setDescription(`**${reactedMember} удалил багнутый запрос роли**`),
     );
 
     // Если есть данные о запросе, поставить статус как удаленный
@@ -30,13 +25,13 @@ exports.run = async ({
 
   // Проверим права автора реакции
   if (
-    !reactedMember.hasPermission("MANAGE_ROLES") &&
-    !reactedMember.roles.cache.some((role) => tagInfo.manage_roles.includes(role.id))
+    !reactedMember.hasPermission('MANAGE_ROLES') &&
+    !reactedMember.roles.cache.some(role => tagInfo.manage_roles.includes(role.id))
   ) {
     // Отправим сообщение об ошибке
     sendErrorMessage({
       message,
-      content: "у вас нет прав на удаление данного запроса",
+      content: 'у вас нет прав на удаление данного запроса',
       member: reactedMember,
       react: false,
       messageType: guildData.give_role.message_type,
@@ -47,18 +42,22 @@ exports.run = async ({
 
   // Удаляем запрос
   message.channel.send(
-    guildData.give_role.message_type == "plain_text"
-      ? // prettier-ignore
-        `**\`[🗑️ | Удаление]\` ${reactedMember} \`удалил запрос пользователя\` ${requestAuthor
-			} \`с ником ${requestInfo.user.nick_info[0].replace(/[`|"|*]/gi, "")}\`**`
+    guildData.give_role.message_type === 'plain_text'
+      ? // eslint-disable-next-line max-len
+        `**\`[🗑️ | Удаление]\` ${reactedMember} \`удалил запрос пользователя\` ${requestAuthor} \`с ником ${requestInfo.user.nick_info[0].replace(
+          /[`|"|*]/gi,
+          '',
+        )}\`**`
       : new MessageEmbed()
-          .setColor("#b4c1d6")
-          .setTitle("**🗑️ | Удаление запроса**")
+          .setColor('#b4c1d6')
+          .setTitle('**🗑️ | Удаление запроса**')
           .setDescription(
-            `**${reactedMember} удалил запрос пользователя ${ 
-				requestAuthor} с ником \`${requestInfo.user.nick_info[0]
-					.replace(/[`|"|*]/gi, "")}\`**` // prettier-ignore
-          )
+            // eslint-disable-next-line max-len
+            `**${reactedMember} удалил запрос пользователя ${requestAuthor} с ником \`${requestInfo.user.nick_info[0].replace(
+              /[`|"|*]/gi,
+              '',
+            )}\`**`,
+          ),
   );
   requestInfo.remove();
   return message.delete();
